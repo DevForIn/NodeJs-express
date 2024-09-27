02a7eb2c12c461bd9bacba098e618818ddafc448
JeongJeongIn <devforin@gmail.com>
2024-09-27 13:40:04 +0900

    postgresql

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -2,14 +2,22 @@ const jwt = require('jsonwebtoken');
 
 // 토큰 인증 미들웨어
 const authenticateToken = (req,res,next) => {
+
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
+
     if(!token) return res.sendStatus(401);
+
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+
         if(err) return res.sendStatus(403);
+
         req.user = user;
         next();
     });
 };
 
 module.exports = authenticateToken;
+
+
+
