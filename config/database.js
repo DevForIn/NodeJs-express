98bb2389c480f8a4152260a1d5ada52e4b4bc152
JeongJeongIn <devforin@gmail.com>
2024-10-28 23:41:45 +0900

    DB connect test Complate

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
 
+// 데이터베이스 설정
+const dbName = 'postgres';
+const dbUser = 'postgres';
+const dbPassword = 'j2in';
+const dbHost = '127.0.0.1';
+
 // Sequelize 인스턴스 생성
-const sequelize = new Sequelize('postgres', 'postgres', 'admin', {
-    host: '127.0.0.1',
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
     dialect: 'postgres',
 });
 
