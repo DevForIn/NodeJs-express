d5cbe004b8645b01e3f4730bec4057ca4cee1030
JeongJeongIn <devforin@gmail.com>
2024-11-04 23:10:03 +0900

    db config setting

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 // 데이터베이스 설정
 const dbName = 'postgres';
 const dbUser = 'postgres';
-const dbPassword = 'admin';
+const dbPassword = 'j2in';
 const dbHost = '127.0.0.1';
 
 // Sequelize 인스턴스 생성
